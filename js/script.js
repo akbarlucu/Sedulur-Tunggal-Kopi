d2f5565c88e6d6180bc0ca5053f74536a76260f6
akbarlucu <rachmadrizqiakbarsyah@gmail.com>
2025-10-09 17:18:04 +0700

    add image file

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 // Toggle class active
 const NavbarNav = document.querySelector(".navbar-nav");
+
+//When hamburger menu get clicked
+document.querySelector("#hamburger-menu").onclick = () => {
+  NavbarNav.classList.toggle("active");
+};
+
+//Click outside navbar to hide hamburger menu
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !NavbarNav.contains(e.target)) {
+    NavbarNav.classList.remove("active");
+  }
+});
